95f25ed5100e42d366386203aaa4c5f153b2cb54
Jonathan Dominguez <jonathan24.dp@gmail.com>
2024-03-11 22:49:12 -0600

    Add title and colors config

---
+++ .assets/print-title.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# ------------------------------------------------------
+# Print the title of the script
+# ------------------------------------------------------
+
+clear
+
+echo -e "${BLUE}"
+figlet -f slant -t -c "HYPRLAND DOTFILES"
+echo -e "${NOCOLOR}"
+
+# Ascii Font -> Mini
+echo -e "${WHITE}"
+figlet -f small -t -c "VERSION" $version
+
+figlet -f small -t -c "BY THE RIAL NIELS"
+echo -e "${NOCOLOR}"
+
---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+version='1.0'
+
+source .assets/colors.sh
+
+source .assets/print-title.sh
+
