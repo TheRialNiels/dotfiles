66d5ad5bd6f27028a832d4995cfc5df62bc70f18
Jonathan Dominguez <jonathan24.dp@gmail.com>
2024-03-16 11:20:15 -0600

    Code refactoring

--- install.sh
+++ install.sh
@@ -2,25 +2,34 @@
 
 version='1.0'
 
+utils_path="utils/"
+inst_path="installation/"
+
 # Load some utils that will be used in the scripts
-source utils/utils.sh
-source utils/colors.sh
+source "${utils_path}utils.sh"
+source "${utils_path}colors.sh"
 
 # Check if the script is running with sudo privileges
 #_checkSudo
 
 # Check some packages before begin with the installation
-source installation/before_installation.sh
+source "${inst_path}before-installation.sh"
 
-source utils/print-title.sh
+source "${utils_path}print-title.sh"
 
-source installation/confirm_installation.sh
+source "${inst_path}confirm-installation.sh"
 
-source installation/yay.sh
+source "${inst_path}yay.sh"
 
-source installation/backup.sh
+source "${inst_path}backup.sh"
 
-source installation/preparation.sh
+source "${inst_path}preparation.sh"
 
-source installation/remove.sh
+source "${inst_path}installer.sh"
+
+source "${inst_path}remove.sh"
+
+source "${inst_path}packages/general-packages.sh"
+
+source "${inst_path}install-packages.sh"
 
