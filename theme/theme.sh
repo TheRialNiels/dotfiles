684e9253df7562dca6bb9c9d5163ef7725816c7a
Jonathan Dominguez <jonathan24.dp@gmail.com>
2024-03-22 23:50:49 -0600

    Refactoring theme script adding some utils in a diff file

--- theme/theme.sh
+++ theme/theme.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-## Copyright (C) 2020-2023 Aditya Shakya <adi1090x@gmail.com>
-##
-## Script To Apply Themes
-
-## Theme ------------------------------------
+# Directories
 DIR="$HOME/dotfiles"
 
-## Directories ------------------------------
+PATH_WALL="$DIR/wallpapers"
 PATH_KITT="$DIR/kitty"
 PATH_FOOT="$DIR/foot"
 PATH_ROFI="$DIR/rofi"
@@ -15,160 +11,81 @@ PATH_WAYB="$DIR/waybar"
 PATH_WLOG="$DIR/wlogout"
 PATH_TMPL="$DIR/theme/templates"
 
-## Source Theme File ------------------------
+# Hypr Scripts
+HYPR_SCRIPTS="$DIR/hypr/scripts"
+
+# Load utils script
+source $DIR/theme/utils.sh
+
+# Theme Sources
 CURRENT_THEME="$DIR/theme/current.bash"
-DEFAULT_THEME="$DIR/theme/default.bash"
 PYWAL_THEME="$HOME/.cache/wal/colors.sh"
 
-## Check if current file exist
-if [[ ! -e "$CURRENT_THEME" ]]; then
-	touch "$CURRENT_THEME"
+# Wallpapers Variables
+CACHE_WALL="$HOME/.cache/current_wallpaper"
+CACHE_WALL_RASI="$HOME/.cache/current_wallpaper.rasi"
+DEFAULT_WALL="$PATH_WALL/default.jpg"
+
+_checkWallpapers $PATH_WALL
+
+## Create cache_wall file if not exists
+if [ ! -f $CACHE_WALL ] ;then
+    touch $CACHE_WALL
+    echo "$DEFAULT_WALL" > "$CACHE_WALL"
 fi
 
-## Default Theme
-source_default() {
-	cat ${DEFAULT_THEME} > ${CURRENT_THEME}
-	source ${CURRENT_THEME}
-	altbackground="`pastel color $background | pastel lighten 0.10 | pastel format hex`"
-	altforeground="`pastel color $foreground | pastel darken 0.30 | pastel format hex`"
-	modbackground=(`pastel gradient -n 3 $background $altbackground | pastel format hex`)
-	accent="$color5"
-	#TODO (Send notification) notify-send -h string:x-canonical-private-synchronous:sys-notify-dtheme -u normal -i ${PATH_MAKO}/icons/palette.png "Applying Default Theme..."
-}
+## Create cache_wall_rasi file if not exists
+if [ ! -f $CACHE_WALL_RASI ] ;then
+    touch $CACHE_WALL_RASI
+    echo "* { current-image: url(\"$DEFAULT_WALL\", height); }" > "$CACHE_WALL_RASI"
+fi
 
-## Random Theme
-source_pywal() {
-	# Set you wallpaper directory here.
-	WALLDIR="$DIR/wallpapers"
+current_wall=$(cat "$CACHE_WALL")
 
-	# Check for wallpapers
-	check_wallpaper() {
-		if [[ -d "$WALLDIR" ]]; then
-			WFILES="`ls --format=single-column $WALLDIR | wc -l`"
-			if [[ "$WFILES" == 0 ]]; then
-				#TODO (Send notification) notify-send -h string:x-canonical-private-synchronous:sys-notify-noimg -u low -i ${PATH_MAKO}/icons/picture.png "There are no wallpapers in : $WALLDIR"
-				exit
-			fi
-		else
-			mkdir -p "$WALLDIR"
-			#TODO (Send notification) notify-send -h string:x-canonical-private-synchronous:sys-notify-noimg -u low -i ${PATH_MAKO}/icons/picture.png "Put some wallpapers in : $WALLDIR"
-			exit
-		fi
-	}
+## Verify that pywal is installed
+if [[ `which wal` ]]; then
+    case $1 in
+        "default")
+            notification_msg="Applying default theme..."
 
-	# Run `pywal` to generate colors
-	generate_colors() {
-		check_wallpaper
-		if [[ `which wal` ]]; then
-			#TODO (Send notification) notify-send -t 50000 -h string:x-canonical-private-synchronous:sys-notify-runpywal -i ${PATH_MAKO}/icons/timer.png "Generating Colorscheme. Please wait..."
-			wal -q -n -s -t -e -i "$WALLDIR"
-			if [[ "$?" != 0 ]]; then
-				#TODO (Send notification) notify-send -h string:x-canonical-private-synchronous:sys-notify-runpywal -u normal -i ${PATH_MAKO}/icons/palette.png "Failed to generate colorscheme."
-				exit
-			fi
-		else
-			#TODO (Send notification) notify-send -h string:x-canonical-private-synchronous:sys-notify-runpywal -u normal -i ${PATH_MAKO}/icons/palette.png "'pywal' is not installed."
-			exit
-		fi
-	}
+            _applyPyWal "$notification_msg" "$DEFAULT_WALL"
+        ;;
 
-	generate_colors
-	cat ${PYWAL_THEME} > ${CURRENT_THEME}
-	source ${CURRENT_THEME}
-	altbackground="`pastel color $background | pastel lighten 0.10 | pastel format hex`"
-	altforeground="`pastel color $foreground | pastel darken 0.30 | pastel format hex`"
-	modbackground=(`pastel gradient -n 3 $background $altbackground | pastel format hex`)
-	accent="$color4"
-}
+        "pywal")
+            notification_msg="Generating colorscheme..."
 
-## Wallpaper ---------------------------------
-apply_wallpaper() {
-	sed -i -e "s#WALLPAPER=.*#WALLPAPER='$wallpaper'#g" ${DIR}/hypr/scripts/wallpaper.sh
-	bash ${DIR}/hypr/scripts/wallpaper.sh &
-}
+            selected_image=$(_selectRandomImage "$PATH_WALL")
+            _applyPyWal "$notification_msg" "$selected_image"
+        ;;
 
-## Kitty ---------------------------------
-apply_kitty() {
-    # kitty : colors
-    cat "${PATH_TMPL}/kitty/color.ini" | envsubst > "${PATH_KITT}/color.ini"
-}
+        *)
+            echo "Available Options: default  pywal"
+		    exit 1
+        ;;
+    esac
 
-## Foot --------------------------------------
-apply_foot() {
-	# foot : colors
-	process_template "${PATH_TMPL}/foot/colors.ini" > "${PATH_FOOT}/colors.ini"
-}
+    ## Get and generate colorscheme variables
+    cat ${PYWAL_THEME} > ${CURRENT_THEME}
+    source ${CURRENT_THEME}
+    altbackground="`pastel color $background | pastel lighten 0.10 | pastel format hex`"
+    altforeground="`pastel color $foreground | pastel darken 0.30 | pastel format hex`"
+    modbackground=(`pastel gradient -n 3 $background $altbackground | pastel format hex`)
+    accent="$color4"
 
-## Rofi --------------------------------------
-apply_rofi() {
-	# rofi : colors
-	cat "${PATH_TMPL}/rofi/colors.rasi" > "${PATH_ROFI}/theme/colors.rasi"
-}
+    ## Export variables
+    export background foreground accent color0 color1 color2 color3 color4 color5 color6 color7 color8 color9 color10 color11 color12 color13 color14 color15
+    export modbackground1="${modbackground[1]}"
+    export modbackground2="${modbackground[2]}"
 
-## Waybar ------------------------------------
-apply_waybar() {
-	# waybar : colors
-	cat "${PATH_TMPL}/waybar/colors.css" | envsubst > "${PATH_WAYB}/colors.css"
-	sleep 1
-	bash ${DIR}/hypr/scripts/statusbar.sh &
-}
-
-## Wlogout -----------------------------------
-apply_wlogout() {
-	# wlogout : colors
-	cat "${PATH_TMPL}/waybar/colors.css" | envsubst > "${PATH_WLOG}/colors.css"
-}
-
-## Hyprland --------------------------------------
-apply_hypr() {
-	# hyprland : theme
-	sed -i ${DIR}/hypr/hyprtheme.conf \
-		-e "s/\$active_border_col_1 =.*/\$active_border_col_1 = 0xFF${accent:1}/g" \
-		-e "s/\$active_border_col_2 =.*/\$active_border_col_2 = 0xFF${color1:1}/g" \
-		-e "s/\$inactive_border_col_1 =.*/\$inactive_border_col_1 = 0xFF${modbackground[1]:1}/g" \
-		-e "s/\$inactive_border_col_2 =.*/\$inactive_border_col_2 = 0xFF${modbackground[2]:1}/g" \
-		-e "s/\$group_border_col =.*/\$group_border_col = 0xFF${color1:1}/g" \
-		-e "s/\$group_border_active_col =.*/\$group_border_active_col = 0xFF${color2:1}/g"
-}
-
-## Replace custom colors variable (eg. color0:1)
-process_template() {
-    local template_file="$1"
-    local output_file="$2"
-
-    # Read the content of the template and process it
-    while IFS= read -r line; do
-        # Replace variables according to your needs
-        for ((i=0; i<=15; i++)); do
-            line="${line//\${color${i}:1}/${!color${i}:1}}"
-        done
-        # You can add more replacements as needed
-
-        # Write the processed line to the output file
-        echo "$line" >> "$output_file"
-    done < "$template_file"
-}
-
-## Source Theme Accordingly -----------------
-if [[ "$1" == '--default' ]]; then
-	source_default
-elif [[ "$1" == '--pywal' ]]; then
-	source_pywal
+    ## Copy coloschemes to the templates
+    _updateWallpaper $wallpaper $CACHE_WALL "$HYPR_SCRIPTS/wallpaper.sh"
+    _applyKittyTemplate $PATH_TMPL $PATH_KITT
+    #TODO _applyFootTemplate $PATH_TMPL $PATH_FOOT
+    #TODO _applyRofiTemplate $PATH_TMPL $PATH_ROFI
+    _applyWaybarTemplate $PATH_TMPL $PATH_WAYB "$HYPR_SCRIPTS/statusbar.sh"
+    #TODO _applyWlogoutTemplate $PATH_TMPL $PATH_WLOG
+    _applyHyprTemplate
 else
-	echo "Available Options: --default  --pywal"
-	exit 1
+    #TODO (Send notification) notify-send -h string:x-canonical-private-synchronous:sys-notify-runpywal -u normal -i ${PATH_MAKO}/icons/palette.png "'pywal' is not installed."
+	exit
 fi
-
-## Export variables
-export background foreground accent color0 color1 color2 color3 color4 color5 color6 color7 color8 color9 color10 color11 color12 color13 color14 color15
-export modbackground1="${modbackground[1]}"
-export modbackground2="${modbackground[2]}"
-
-## Execute Script ---------------------------
-apply_wallpaper
-apply_kitty
-#TODO apply_foot
-#TODO apply_rofi
-apply_waybar
-#TODO apply_wlogout
-apply_hypr
---
+++ theme/utils.sh
@@ -0,0 +1,137 @@
+_checkWallpapers() {
+    local path=$1
+
+	if [[ -d "$path" ]]; then
+		files="`ls --format=single-column $path | wc -l`"
+		if [[ "$files" == 0 ]]; then
+			#TODO (Send notification) notify-send -h string:x-canonical-private-synchronous:sys-notify-noimg -u low -i ${PATH_MAKO}/icons/picture.png "There are no wallpapers in : $path"
+            notify-send "There are no wallpapers in : $path"
+			exit
+		fi
+	else
+		mkdir -p "$path"
+		#TODO (Send notification) notify-send -h string:x-canonical-private-synchronous:sys-notify-noimg -u low -i ${PATH_MAKO}/icons/picture.png "Put some wallpapers in : $path"
+        notify-send "Put some wallpapers in : $path"
+		exit
+	fi
+}
+
+_applyPyWal() {
+    local notification_msg=$1
+    local folder_image_path=$2
+
+    #TODO (Send notification) notify-send -h string:x-canonical-private-synchronous:sys-notify-dtheme -u normal -i ${PATH_MAKO}/icons/palette.png "Applying Default Theme..."
+    notify-send "$notification_msg"
+    wal -q -n -s -t -e -i $folder_image_path
+
+    if [[ "$?" != 0 ]]; then
+        #TODO (Send notification) notify-send -h string:x-canonical-private-synchronous:sys-notify-runpywal -u normal -i ${PATH_MAKO}/icons/palette.png "Failed to generate colorscheme."
+        notify-send "Failed to generate colorscheme"
+        exit
+    fi
+}
+
+_selectRandomImage() {
+    local folder_path="$1"
+    local images=()
+
+    # Iterate through the files in the folder
+    for file in "$folder_path"/*; do
+        # Check if the file is an image (jpg, jpeg, or png extension)
+        if [[ $file =~ \.(jpg|jpeg|png)$ ]]; then
+            images+=("$file")
+        fi
+    done
+
+    # Check if any images were found in the folder
+    if [[ ${#images[@]} -eq 0 ]]; then
+        #TODO Add style to the notification
+        notify-send "No wallpapers found in $folder_path"
+        echo "No Wallpapers"
+    else
+        # Select a random image from the list
+        local selected_image="${images[RANDOM % ${#images[@]}]}"
+        echo "$selected_image"
+    fi
+}
+
+## Replace custom colors variable (eg. color0:1)
+_processTemplate() {
+    local template_file="$1"
+    local output_file="$2"
+
+    # Read the content of the template and process it
+    while IFS= read -r line; do
+        # Replace variables according to your needs
+        for ((i=0; i<=15; i++)); do
+            line="${line//\${color${i}:1}/${!color${i}:1}}"
+        done
+        # You can add more replacements as needed
+
+        # Write the processed line to the output file
+        echo "$line" >> "$output_file"
+    done < "$template_file"
+}
+
+_updateWallpaper() {
+    local wallpaper=$1
+    local destination_file=$2
+    local script_file_path=$3
+
+    echo "$wallpaper" > "$destination_file"
+	bash $script_file_path &
+}
+
+_applyKittyTemplate() {
+    local tmpl_path=$1
+    local kitty_path=$2
+
+    # kitty : colors
+    cat "${tmpl_path}/kitty/color.ini" | envsubst > "${kitty_path}/color.ini"
+}
+
+_applyFootTemplate() {
+    local tmpl_path=$1
+    local foot_path=$2
+
+	# foot : colors
+	process_template "${tmpl_path}/foot/colors.ini" > "${foot_path}/colors.ini"
+}
+
+_applyRofiTemplate() {
+    local tmpl_path=$1
+    local rofi_path=$2
+
+	# rofi : colors
+	cat "${tmpl_path}/rofi/colors.rasi" > "${rofi_path}/theme/colors.rasi"
+}
+
+_applyWaybarTemplate() {
+    local tmpl_path=$1
+    local wayb_path=$2
+    local script_file_path=$3
+
+	# waybar : colors
+	cat "${tmpl_path}/waybar/colors.css" | envsubst > "${wayb_path}/colors.css"
+	sleep 1
+	bash $script_file_path &
+}
+
+_applyWlogoutTemplate() {
+    local tmpl_path=$1
+    local wlog_path=$2
+
+	# wlogout : colors (Is the same file as waybar)
+	cat "${tmpl_path}/waybar/colors.css" | envsubst > "${wlog_path}/colors.css"
+}
+
+_applyHyprTemplate() {
+	# hyprland : theme
+	sed -i ~/dotfiles/hypr/hyprtheme.conf \
+		-e "s/\$active_border_col_1 =.*/\$active_border_col_1 = 0xFF${accent:1}/g" \
+		-e "s/\$active_border_col_2 =.*/\$active_border_col_2 = 0xFF${color1:1}/g" \
+		-e "s/\$inactive_border_col_1 =.*/\$inactive_border_col_1 = 0xFF${modbackground[1]:1}/g" \
+		-e "s/\$inactive_border_col_2 =.*/\$inactive_border_col_2 = 0xFF${modbackground[2]:1}/g" \
+		-e "s/\$group_border_col =.*/\$group_border_col = 0xFF${color1:1}/g" \
+		-e "s/\$group_border_active_col =.*/\$group_border_active_col = 0xFF${color2:1}/g"
+}
