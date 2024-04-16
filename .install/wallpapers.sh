2accf2d0854834c4af66dd52928110d994af0f19
Jonathan Dominguez <jonathan24.dp@gmail.com>
2024-04-16 12:47:51 -0600

    Refact code

--- .install/wallpapers.sh
+++ .install/wallpapers.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # ------------------------------------------------------
 # Copy default wallpaper files to .cache
@@ -9,14 +9,15 @@ cache_file="$HOME/.cache/current_wallpaper"
 rasi_file="$HOME/.cache/current_wallpaper.rasi"
 
 # Create cache file if not exists
-if [ ! -f $cache_file ] ;then
-    touch $cache_file
-    echo "$HOME/dotfiles/wallpapers/default.jpg" > "$cache_file"
+if [ ! -f "$cache_file" ]; then
+    touch "$cache_file"
+    echo "$DOTFILES/wallpapers/default.jpg" >"$cache_file"
+    _showSuccessMsg "Cache file for default wallpaper created"
 fi
 
 # Create rasi file if not exists
-if [ ! -f $rasi_file ] ;then
-    touch $rasi_file
-    echo "* { current-image: url(\"$HOME/dotfiles/wallpapers/default.jpg\", height); }" > "$rasi_file"
+if [ ! -f "$rasi_file" ]; then
+    touch "$rasi_file"
+    echo "* { current-image: url(\"$DOTFILES/wallpapers/default.jpg\", height); }" >"$rasi_file"
+    _showSuccessMsg "Cache file for default rasi wallpaper created"
 fi
-
