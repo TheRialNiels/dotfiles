ec8725d53257198c68dbb5fe4ec162c6ec30c216
Jonathan Dominguez <jonathan24.dp@gmail.com>
2024-04-15 22:27:38 -0600

    Add config to change shell to root user

--- .install/zsh.sh
+++ .install/zsh.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 echo -e "${BLUE}"
-    figlet "zsh"
+figlet "zsh"
 echo -e "${NOCOLOR}"
 
-# Change to SHELL to zsh
-chsh -s /bin/zsh
+# Change to SHELL to zsh to current user
+chsh -s /bin/zsh && echo -e "\n"
 
-# Install Zap, ZSH plugin manager
-zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+# Change to SHELL to zsh to root user
+sudo chsh -s /bin/zsh && echo -e "\n"
+
+# Install Zap, ZSH plugin manager to current user
+zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1 && echo -e "\n"
+
+# Install Zap, ZSH plugin manager to root user
+_showNormalMsg "Now insert the password to access to root user in order to install Zap (ZSH plugin manager)"
+
+su -c "zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1 && echo -e '\n'"
