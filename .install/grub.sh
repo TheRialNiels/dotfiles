5ea603e0756e9cd15fa72e7ff9e4ae38fa5a6cf3
Jonathan Dominguez <jonathan24.dp@gmail.com>
2024-04-17 08:24:40 -0600

    Delete script path lines

--- .install/grub.sh
+++ .install/grub.sh
@@ -4,11 +4,7 @@ echo -e "${BLUE}"
 figlet "Grub"
 echo -e "${NOCOLOR}"
 
-SCRIPT=$(realpath "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-
 cd "$HOME" || return
 git clone https://github.com/vinceliuice/grub2-themes.git
 cd grub2-themes || return
 sudo ./install.sh -t vimix
-cd "$SCRIPTPATH" || return
