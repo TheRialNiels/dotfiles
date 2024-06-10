5dec0bc467c332efc920b673308cd862af02e7d3
Jonathan Dominguez <jonathan24.dp@gmail.com>
2024-06-09 19:30:52 -0600

    Add space

--- .install/wallpapers.sh
+++ .install/wallpapers.sh
@@ -11,13 +11,13 @@ rasi_file="$HOME/.cache/current_wallpaper.rasi"
 # Create cache file if not exists
 if [ ! -f "$cache_file" ]; then
     touch "$cache_file"
-    echo "$DOTFILES/wallpapers/default.jpg" >"$cache_file"
+    echo "$DOTFILES/wallpapers/default.jpg" > "$cache_file"
     _showSuccessMsg "Cache file for default wallpaper created"
 fi
 
 # Create rasi file if not exists
 if [ ! -f "$rasi_file" ]; then
     touch "$rasi_file"
-    echo "* { current-image: url(\"$DOTFILES/wallpapers/default.jpg\", height); }" >"$rasi_file"
+    echo "* { current-image: url(\"$DOTFILES/wallpapers/default.jpg\", height); }" > "$rasi_file"
     _showSuccessMsg "Cache file for default rasi wallpaper created"
 fi
