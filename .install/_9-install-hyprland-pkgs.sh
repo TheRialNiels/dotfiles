abf2fa270aa3169e8b21336a3362f25bbe4ce02c
Jonathan Dominguez <jonathan24.dp@gmail.com>
2024-06-27 23:29:58 -0600

    Add script to install hyprland packages

---
+++ .install/_9-install-hyprland-pkgs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# -----------------------------------------------------
+# Install Hyprland Packages
+#
+# This script installs the hyprland packages.
+#
+# Made by TheRialNiels
+# -----------------------------------------------------
+
+## Source Utils
+# shellcheck source=../scripts/messages-utils.sh
+source "$SCRIPTS_DIR/messages-utils.sh"
+
+# shellcheck source=../scripts/packages-utils.sh
+source "$SCRIPTS_DIR/packages-utils.sh"
