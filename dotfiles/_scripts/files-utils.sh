ca5c4e13a2492a58110ee2b10a12ca264f2da9cd
Jonathan Dominguez <jonathan24.dp@gmail.com>
2024-07-07 22:02:02 -0600

    chore: Refactor files-utils.sh and add _replaceLineInTemplate function

--- dotfiles/_scripts/files-utils.sh
+++ dotfiles/_scripts/files-utils.sh
@@ -5,9 +5,6 @@
 # This script contains functions to work with files.
 # -----------------------------------------------------
 
-# shellcheck source=./messages-utils.sh
-source "./messages-utils.sh"
-
 # -----------------------------------------------------
 # _replaceLineIfUnchanged - Replaces a line in a file if
 # it is unchanged.
@@ -102,6 +99,51 @@ _replaceWordIfUnchanged() {
 }
 
 # -----------------------------------------------------
+# _replaceLineInTemplate - Replaces a line in a template
+# file if it is unchanged.
+#
+# Usage:
+#   _replaceLineInTemplate <templatePath> <cachePath> <fileName> <oldLine> <newLine>
+#
+# Parameters:
+#   templatePath: The path to the template file.
+#   cachePath: The path to the cache file.
+#   fileName: The name of the file.
+#   oldLine: The line to replace.
+#   newLine: The new line to insert.
+#
+# Example:
+#   _replaceLineInTemplate "$HOME/.config" "$HOME/.cache" "file.txt" "old line" "new line"
+# -----------------------------------------------------
+_replaceLineInTemplate() {
+    local templatePath="$1"
+    local cachePath="$2"
+    local fileName="$3"
+    local oldLine="$4"
+    local newLine="$5"
+
+    if [[ -z "$templatePath" || -z "$cachePath" || -z "$fileName" || -z "$oldLine" || -z "$newLine" ]]; then
+        echo "Usage: replace_line_in_template <templatePath> <cachePath> <fileName> <oldLine> <newLine> [runAsSudo]"
+        return 1
+    fi
+
+    local templateFile="${templatePath}/${fileName}"
+    local cacheFile="${cachePath}/${fileName}"
+
+    if [[ ! -f "$templateFile" ]]; then
+        _message "error" "Template file '${templateFile}' does not exist."
+        return 1 # Return error
+    fi
+
+    if [[ ! -f "$cacheFile" ]]; then
+        _message "error" "Cache file '${cacheFile}' does not exist."
+        return 1 # Return error
+    fi
+
+    _replaceLineIfUnchanged "$cacheFile" "$oldLine" "$newLine"
+}
+
+# -----------------------------------------------------
 # _createCacheFile - Creates a cache file if it does not
 # exist.
 #
@@ -119,7 +161,7 @@ _createCacheFile() {
     local filePath=$1
     local content=$2
     if [ ! -f "$filePath" ]; then
-        echo "$content" > "$filePath"
+        echo "$content" >"$filePath"
         _message "success" "'$(basename "$filePath")' created"
     fi
 }
