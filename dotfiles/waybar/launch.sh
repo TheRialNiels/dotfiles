821a06464da2930c50fd7e5cb2fab431f1bfd5f1
Jonathan Dominguez <jonathan24.dp@gmail.com>
2024-07-20 12:51:51 -0600

    Refactor Waybar launch script to add sensor script and improve error logging

--- dotfiles/waybar/launch.sh
+++ dotfiles/waybar/launch.sh
@@ -9,9 +9,11 @@ killall waybar
 pkill waybar
 sleep 0.2
 
+## TODO - Add script to define sensors
+
 ## Define the Waybar configuration files
 config="$DOTFILES/waybar/config.jsonc"
 style="$DOTFILES/waybar/style.css"
 
 ## Launch Waybar with the specified configuration
-waybar -c "$config" -s "$style" &
+waybar --log-level error -c "$config" -s "$style"
