86e8758f39b1d9cc27c2b7a9c9e03f5afca2dffb
Jonathan Dominguez <jonathan24.dp@gmail.com>
2025-12-09 20:44:50 -0600

    Empty folder

--- .install/grub.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo -e "${BLUE}"
-figlet "Grub"
-echo -e "${NOCOLOR}"
-
-git clone https://github.com/vinceliuice/grub2-themes.git
-cd grub2-themes || return
-sudo ./install.sh -t vimix
