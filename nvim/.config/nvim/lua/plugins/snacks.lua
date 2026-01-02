b22aebdabffa6d250b1b5ac1e496b9a61c7f7941
Jonathan Dominguez <jonathan24.dp@gmail.com>
2026-01-01 22:10:16 -0600

    Modify keys

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,26 +1,43 @@
 return {
-	"folke/snacks.nvim",
-	priority = 1000,
-	lazy = false,
-	opts = {
-		explorer = {
-			enabled = true,
-			hidden = true,        -- Show hidden files in explorer
-			ignored = false,      -- Do NOT hide gitignored files
-		},
-		picker = {
-			enabled = true,
-			hidden = true,        -- Show dotfiles everywhere
-			ignored = false,      -- Include gitignored files
-		},
-	},
-	keys = {
-		-- Top Pickers & Explorer
-		{ "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
-		{ "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
-		{ "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
-		-- { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
-		-- { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
-		{ "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },},
-	}
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  dependencies = {
+    "echasnovski/mini.icons",
+  },
+  opts = {
+    dashboard = {
+      enabled = true,
+      preset = {
+        header = [[
+                                                                     
+       ████ ██████           █████      ██                     
+      ███████████             █████                             
+      █████████ ███████████████████ ███   ███████████   
+     █████████  ███    █████████████ █████ ██████████████   
+    █████████ ██████████ █████████ █████ █████ ████ █████   
+  ███████████ ███    ███ █████████ █████ █████ ████ █████  
+ ██████  █████████████████████ ████ █████ █████ ████ ██████ 
+        ]],
+      },
+    },
+    explorer = {
+      enabled = true,
+      hidden = true,        -- Show hidden files in explorer
+      ignored = false,      -- Do NOT hide gitignored files
+    },
+    picker = {
+      enabled = true,
+      hidden = true,        -- Show dotfiles everywhere
+      ignored = false,      -- Include gitignored files
+    },
+  },
+  keys = {
+    -- TODO -> Move this to keybindings file
+    { "<leader><space>",  function() Snacks.picker.smart()      end, desc = "Smart Find Files"      },
+    { "<leader>fb",       function() Snacks.picker.buffers()    end, desc = "Buffers"               },
+    { "<leader>fg",       function() Snacks.picker.grep()       end, desc = "Grep"                  },
+    { "<leader>e",        function() Snacks.explorer()          end, desc = "File Explorer"         },
+  },
+}
 
