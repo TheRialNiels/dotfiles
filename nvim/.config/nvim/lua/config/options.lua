f273b57cf169dd58985a3ea2132d155dd3ff95c6
Jonathan Dominguez <jonathan24.dp@gmail.com>
2026-01-12 23:09:02 -0600

    Update nvim config

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,21 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Enable visual line wrapping
+vim.opt.wrap = true
+
+-- Wrap at word boundaries rather than splitting words
+vim.opt.linebreak = true
+
+-- Display a character (e.g., a curved arrow) where the line visually breaks
+vim.opt.showbreak = "↪ "
+
+-- Ensure that whitespace characters are not displayed as text, which can interfere with wrapping
+vim.opt.list = false
+
+-- Set the maximum line length
+vim.opt.textwidth = 80
+
+-- Automatically insert newlines when typing past the textwidth limit
+vim.opt.formatoptions:append("t")
