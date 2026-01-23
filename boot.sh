fe50e6295036465dc0d7ba50c9a1443042048c95
Jonathan Dominguez <jonathan24.dp@gmail.com>
2026-01-23 05:11:51 +0000

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,46 +1,44 @@
 #!/bin/bash
 
 # Set install mode to online since boot.sh is used for curl installations
-export OMARCHY_ONLINE_INSTALL=true
+export TRND_ONLINE_INSTALL=true
 
-ansi_art='                 ▄▄▄
- ▄█████▄    ▄███████████▄    ▄███████   ▄███████   ▄███████   ▄█   █▄    ▄█   █▄
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   █▀   ███   ███  ███   ███
-███   ███  ███   ███   ███ ▄███▄▄▄███ ▄███▄▄▄██▀  ███       ▄███▄▄▄███▄ ███▄▄▄███
-███   ███  ███   ███   ███ ▀███▀▀▀███ ▀███▀▀▀▀    ███      ▀▀███▀▀▀███  ▀▀▀▀▀▀███
-███   ███  ███   ███   ███  ███   ███ ██████████  ███   █▄   ███   ███  ▄██   ███
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
- ▀█████▀    ▀█   ███   █▀   ███   █▀   ███   ███  ███████▀   ███   █▀    ▀█████▀
-                                       ███   █▀                                  '
+# Sub-zero
+ansi_art='
+ ______   ______     __   __     _____                                         
+/\__  _\ /\  == \   /\ "-.\ \   /\  __-.                                       
+\/_/\ \/ \ \  __<   \ \ \-.  \  \ \ \/\ \                                      
+   \ \_\  \ \_\ \_\  \ \_\\"\_\  \ \____-                                      
+    \/_/   \/_/ /_/   \/_/ \/_/   \/____/                                      
+'
 
 clear
 echo -e "\n$ansi_art\n"
 
 sudo pacman -Syu --noconfirm --needed git
 
-# Use custom repo if specified, otherwise default to basecamp/omarchy
-OMARCHY_REPO="${OMARCHY_REPO:-basecamp/omarchy}"
+# Use custom repo if specified, otherwise default to basecamp/trnd
+TRND_REPO="${TRND_REPO:-TheRialNiels/dotfiles}"
 
-echo -e "\nCloning Omarchy from: https://github.com/${OMARCHY_REPO}.git"
-rm -rf ~/.local/share/omarchy/
-git clone "https://github.com/${OMARCHY_REPO}.git" ~/.local/share/omarchy >/dev/null
+echo -e "\nCloning TRND from: https://github.com/${TRND_REPO}.git"
+rm -rf ~/.local/share/trnd/
+git clone "https://github.com/${TRND_REPO}.git" ~/.local/share/trnd >/dev/null
 
 # Use custom branch if instructed, otherwise default to master
-OMARCHY_REF="${OMARCHY_REF:-master}"
-if [[ $OMARCHY_REF != "master" ]]; then
-  echo -e "\e[32mUsing branch: $OMARCHY_REF\e[0m"
-  cd ~/.local/share/omarchy
-  git fetch origin "${OMARCHY_REF}" && git checkout "${OMARCHY_REF}"
+TRND_REF="${TRND_REF:-main}"
+if [[ $TRND_REF != "main" ]]; then
+  echo -e "\e[32mUsing branch: $TRND_REF\e[0m"
+  cd ~/.local/share/trnd
+  git fetch origin "${TRND_REF}" && git checkout "${TRND_REF}"
   cd -
 fi
 
 # Set edge mirror for dev installs
-if [[ $OMARCHY_REF == "dev" ]]; then
-  export OMARCHY_MIRROR=edge
+if [[ $TRND_REF == "dev" ]]; then
+  export TRND_MIRROR=edge
 else
-  export OMARCHY_MIRROR=stable
+  export TRND_MIRROR=stable
 fi
 
 echo -e "\nInstallation starting..."
-source ~/.local/share/omarchy/install.sh
+source ~/.local/share/trnd/install.sh
