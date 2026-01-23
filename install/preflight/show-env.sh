dc77145a09bcf0e6dce69d5540987827670be8f5
Jonathan Dominguez <jonathan24.dp@gmail.com>
2026-01-23 05:12:13 +0000

    Change Omarchy variables

--- install/preflight/show-env.sh
+++ install/preflight/show-env.sh
@@ -1,6 +1,6 @@
 # Show installation environment variables
 gum log --level info "Installation Environment:"
 
-env | grep -E "^(OMARCHY_CHROOT_INSTALL|OMARCHY_ONLINE_INSTALL|OMARCHY_USER_NAME|OMARCHY_USER_EMAIL|USER|HOME|OMARCHY_REPO|OMARCHY_REF|OMARCHY_PATH)=" | sort | while IFS= read -r var; do
+env | grep -E "^(TRND_CHROOT_INSTALL|TRND_ONLINE_INSTALL|TRND_USER_NAME|TRND_USER_EMAIL|USER|HOME|TRND_REPO|TRND_REF|TRND_PATH)=" | sort | while IFS= read -r var; do
   gum log --level info "  $var"
 done
