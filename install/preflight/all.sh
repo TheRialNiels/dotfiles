eda4d12dac939f2c72d0320a4d18bb8f6604050e
Jonathan Dominguez <jonathan24.dp@gmail.com>
2026-01-24 00:39:32 +0000

    Change vars

--- install/preflight/all.sh
+++ install/preflight/all.sh
@@ -1,7 +1,7 @@
-source $OMARCHY_INSTALL/preflight/guard.sh
-source $OMARCHY_INSTALL/preflight/begin.sh
-run_logged $OMARCHY_INSTALL/preflight/show-env.sh
-run_logged $OMARCHY_INSTALL/preflight/pacman.sh
-run_logged $OMARCHY_INSTALL/preflight/migrations.sh
-run_logged $OMARCHY_INSTALL/preflight/first-run-mode.sh
-run_logged $OMARCHY_INSTALL/preflight/disable-mkinitcpio.sh
+source $TRND_INSTALL/preflight/guard.sh
+source $TRND_INSTALL/preflight/begin.sh
+run_logged $TRND_INSTALL/preflight/show-env.sh
+run_logged $TRND_INSTALL/preflight/pacman.sh
+run_logged $TRND_INSTALL/preflight/migrations.sh
+run_logged $TRND_INSTALL/preflight/first-run-mode.sh
+run_logged $TRND_INSTALL/preflight/disable-mkinitcpio.sh
