a194e65aa4d442e8bb5019553a06c70df69ed5dd
Jonathan Dominguez <jonathan24.dp@gmail.com>
2026-01-24 00:37:25 +0000

    Update preflight files

--- install/preflight/pacman.sh
+++ install/preflight/pacman.sh
@@ -1,21 +1,15 @@
-if [[ -n ${OMARCHY_ONLINE_INSTALL:-} ]]; then
+if [[ -n ${TRND_ONLINE_INSTALL:-} ]]; then
   # Install build tools
   sudo pacman -S --needed --noconfirm base-devel
 
   # Configure pacman
-  if [[ ${OMARCHY_MIRROR:-} == "edge" ]] ; then
-    sudo cp -f ~/.local/share/omarchy/default/pacman/pacman-edge.conf /etc/pacman.conf
-    sudo cp -f ~/.local/share/omarchy/default/pacman/mirrorlist-edge /etc/pacman.d/mirrorlist
+  if [[ ${TRND_MIRROR:-} == "edge" ]] ; then
+    sudo cp -f ~/.local/share/trnd/default/pacman/pacman-edge.conf /etc/pacman.conf
   else
-    sudo cp -f ~/.local/share/omarchy/default/pacman/pacman-stable.conf /etc/pacman.conf
-    sudo cp -f ~/.local/share/omarchy/default/pacman/mirrorlist-stable /etc/pacman.d/mirrorlist
+    sudo cp -f ~/.local/share/trnd/default/pacman/pacman-stable.conf /etc/pacman.conf
   fi
 
-  sudo pacman-key --recv-keys 40DFB630FF42BCFFB047046CF0134EE680CAC571 --keyserver keys.openpgp.org
-  sudo pacman-key --lsign-key 40DFB630FF42BCFFB047046CF0134EE680CAC571
-
   sudo pacman -Sy
-  sudo pacman -S --noconfirm --needed omarchy-keyring
 
 
   # Refresh all repos
