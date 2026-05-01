6f8b38777fa160233d208cac5f99b538d164fd3f
Jonathan Dominguez <jonathan24.dp@gmail.com>
2026-04-30 22:35:22 -0600

    delete keybindings

--- nvim/.config/nvim/lua/plugins/vim-tmux-navigation.lua
+++ nvim/.config/nvim/lua/plugins/vim-tmux-navigation.lua
@@ -5,19 +5,4 @@ return {
   -- URL: https://github.com/alexghergh/nvim-tmux-navigation
   -- Description: A Neovim plugin that allows seamless navigation between Neovim and tmux panes.
   "alexghergh/nvim-tmux-navigation",
-  cmd = {
-    "TmuxNavigateLeft",
-    "TmuxNavigateDown",
-    "TmuxNavigateUp",
-    "TmuxNavigateRight",
-    "TmuxNavigatePrevious",
-    "TmuxNavigatorProcessList",
-  },
-  keys = {
-    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-  },
 }
