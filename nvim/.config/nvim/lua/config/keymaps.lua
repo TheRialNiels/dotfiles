931d5322f22954d695e3fc4f0564a629d24f6713
Jonathan Dominguez <jonathan24.dp@gmail.com>
2026-05-05 21:02:44 -0600

    refactor keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,72 +2,71 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
+-- Select all
+map("n", "<C-a>", "gg<S-v>G")
+
 -- Map Ctrl+b in insert mode to delete to the end of the word without leaving insert mode
-vim.keymap.set("i", "<C-b>", "<C-o>de")
+map("i", "<C-b>", "<C-o>de")
 
 -- Map Ctrl+c to escape from other modes
-vim.keymap.set({ "i", "n", "v" }, "<C-c>", [[<C-\><C-n>]])
+map({ "i", "n", "v" }, "<C-c>", [[<C-\><C-n>]])
 
 -- Screen Keys
--- vim.keymap.set({ "n" }, "<leader>uk", "<cmd>Screenkey<CR>")
+-- map({ "n" }, "<leader>uk", "<cmd>Screenkey<CR>")
 
 -- Scroll down half a page and center the cursor
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+map("n", "<C-d>", "<C-d>zz", opts)
 
 -- Scroll up half a page and center the cursor
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
+map("n", "<C-u>", "<C-u>zz", opts)
 
 -- Keep search results centered
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
 
 -- Telescope projects
-vim.keymap.set("n", "<leader>fp", "<cmd>Telescope projects<CR>", { desc = "Projects" })
+map("n", "<leader>fp", "<cmd>Telescope projects<CR>", { desc = "Projects" })
 
 ----- Tmux Navigation ------
 local nvim_tmux_nav = require("nvim-tmux-navigation")
 
-vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft) -- Navigate to the left pane
-vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown) -- Navigate to the bottom pane
-vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp) -- Navigate to the top pane
-vim.keymap.set("n", "<C-l", nvim_tmux_nav.NvimTmuxNavigateRight) -- Navigate to the right pane
-vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive) -- Navigate to the last active pane
-vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext) -- Navigate to the next pane
+map("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft) -- Navigate to the left pane
+map("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown) -- Navigate to the bottom pane
+map("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp) -- Navigate to the top pane
+map("n", "<C-l", nvim_tmux_nav.NvimTmuxNavigateRight) -- Navigate to the right pane
+map("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive) -- Navigate to the last active pane
+map("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext) -- Navigate to the next pane
 
 ----- OBSIDIAN -----
--- vim.keymap.set("n", "<leader>oc", "<cmd>ObsidianCheck<CR>", { desc = "Obsidian Check Checkbox" })
--- vim.keymap.set("n", "<leader>ot", "<cmd>ObsidianTemplate<CR>", { desc = "Insert Obsidian Template" })
--- vim.keymap.set("n", "<leader>oo", "<cmd>Obsidian Open<CR>", { desc = "Open in Obsidian App" })
--- vim.keymap.set("n", "<leader>ob", "<cmd>ObsidianBacklinks<CR>", { desc = "Show ObsidianBacklinks" })
--- vim.keymap.set("n", "<leader>ol", "<cmd>ObsidianLinks<CR>", { desc = "Show ObsidianLinks" })
--- vim.keymap.set("n", "<leader>on", "<cmd>ObsidianNew<CR>", { desc = "Create New Note" })
--- vim.keymap.set("n", "<leader>os", "<cmd>ObsidianSearch<CR>", { desc = "Search Obsidian" })
--- vim.keymap.set("n", "<leader>oq", "<cmd>ObsidianQuickSwitch<CR>", { desc = "Quick Switch" })
+-- map("n", "<leader>oc", "<cmd>ObsidianCheck<CR>", { desc = "Obsidian Check Checkbox" })
+-- map("n", "<leader>ot", "<cmd>ObsidianTemplate<CR>", { desc = "Insert Obsidian Template" })
+-- map("n", "<leader>oo", "<cmd>Obsidian Open<CR>", { desc = "Open in Obsidian App" })
+-- map("n", "<leader>ob", "<cmd>ObsidianBacklinks<CR>", { desc = "Show ObsidianBacklinks" })
+-- map("n", "<leader>ol", "<cmd>ObsidianLinks<CR>", { desc = "Show ObsidianLinks" })
+-- map("n", "<leader>on", "<cmd>ObsidianNew<CR>", { desc = "Create New Note" })
+-- map("n", "<leader>os", "<cmd>ObsidianSearch<CR>", { desc = "Search Obsidian" })
+-- map("n", "<leader>oq", "<cmd>ObsidianQuickSwitch<CR>", { desc = "Quick Switch" })
 
 ----- OIL -----
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+map("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- Delete all buffers but the current one
-vim.keymap.set(
-  "n",
-  "<leader>bq",
-  '<Esc>:%bdelete|edit #|normal`"<Return>',
-  { desc = "Delete other buffers but the current one" }
-)
+map("n", "<leader>bq", '<Esc>:%bdelete|edit #|normal`"<Return>', { desc = "Delete other buffers but the current one" })
 
 -- Disable key mappings in visual block mode
-vim.api.nvim_set_keymap("x", "<A-j>", "<Nop>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("x", "<A-k>", "<Nop>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("x", "J", "<Nop>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("x", "K", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("x", "<A-j>", "<Nop>", opts)
+vim.api.nvim_set_keymap("x", "<A-k>", "<Nop>", opts)
+vim.api.nvim_set_keymap("x", "J", "<Nop>", opts)
+vim.api.nvim_set_keymap("x", "K", "<Nop>", opts)
 
 -- Redefine Ctrl+s to save with the custom function
-vim.api.nvim_set_keymap("n", "<C-s>", ":lua SaveFile()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-s>", ":lua SaveFile()<CR>", opts)
 
 -- Grep keybinding for visual mode - search selected text
-vim.keymap.set("v", "<leader>sg", function()
+map("v", "<leader>sg", function()
   -- Get the selected text
   local start_pos = vim.fn.getpos("'<")
   local end_pos = vim.fn.getpos("'>")
@@ -102,7 +101,7 @@ vim.keymap.set("v", "<leader>sg", function()
 end, { desc = "Grep Selected Text" })
 
 -- Grep keybinding for visual mode with G - search selected text at root level
-vim.keymap.set("v", "<leader>sG", function()
+map("v", "<leader>sG", function()
   -- Get git root or fallback to cwd
   local git_root = vim.fn.system("git rev-parse --show-toplevel 2>/dev/null"):gsub("\n", "")
   local root = vim.v.shell_error == 0 and git_root ~= "" and git_root or vim.fn.getcwd()
@@ -141,7 +140,7 @@ vim.keymap.set("v", "<leader>sG", function()
 end, { desc = "Grep Selected Text (Root Dir)" })
 
 -- Delete all marks
-vim.keymap.set("n", "<leader>md", function()
+map("n", "<leader>md", function()
   vim.cmd("delmarks!")
   vim.cmd("delmarks A-Z0-9")
   vim.notify("All marks deleted")
